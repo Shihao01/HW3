b48b2feb6ffd3429551449718d5070cf4aa539ae
Shihao01 <ysz_86@icloud.com>
2019-04-21 18:36:59 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,10 +7,9 @@ function checkEmptyInput()
                 var isEmpty = false,
                     moviename = document.getElementById("moviename").value,
                     rating = document.getElementById("rating").value;
-                    
-            
-                if(moviename == ""){
-                    alert("moviename Connot Be Empty");
+                                
+                if(moviename === ""){
+                    alert("MovieName Connot Be Empty");
                     isEmpty = true;
                 }
                 else if(rating <= 0){
@@ -20,20 +19,16 @@ function checkEmptyInput()
 		else if (rating > 5){
 		alert("rating should be between 1-5 Please remove last input");
 		isEmpty = true;
-				}
-                
+		}             
                 return isEmpty;
             }// a method to validate  if the two fields are valid 
 
 function addmoive()
             {
-         
-		table = document.getElementById("table");
                 if(!checkEmptyInput()){
                 var newRow = table.insertRow(table.length),
                     cell1 = newRow.insertCell(0),
-                    cell2 = newRow.insertCell(1),
-                    
+                    cell2 = newRow.insertCell(1),                   
                     moviename = document.getElementById("moviename").value,
                     rating = document.getElementById("rating").value;
 		    
@@ -46,8 +41,7 @@ function addmoive()
 // add Movie title Name and rating 
   
  function selectedRowToInput()
-            {
-                
+            {              
                 for(var i = 1; i < table.rows.length; i++)
                 {
                     table.rows[i].onclick = function()
@@ -55,23 +49,19 @@ function addmoive()
                       // get the seected row index
                       rIndex = this.rowIndex;
                       document.getElementById("moviename").value = this.cells[0].innerHTML;
-                      document.getElementById("rating").value = this.cells[1].innerHTML;
-                      
+                      document.getElementById("rating").value = this.cells[1].innerHTML;                
                     };
                 }
             }
  selectedRowToInput();   
             
 // property to hold movie title and rating 
-
-
  function removemoive()
             {
                 table.deleteRow(rIndex);
                 // clear input text
                 document.getElementById("moviename").value = "";
-                document.getElementById("rating").value = "";
-                
+                document.getElementById("rating").value = "";               
             }
 // remove data not need 
 
